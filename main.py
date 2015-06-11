4481a02c90d055da119c458ffcaaefbf59268d03
hwgao <hhww.gao@gmail.com>
2015-06-11 12:00:01 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+__author__ = 'hwgao'
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+
+
+class AddLocationForm(BoxLayout):
+    search_input = ObjectProperty()
+
+    def search_location(self):
+        print("The user searched for '{}'".format(self.search_input.text))
+
+
+class WeatherApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    WeatherApp().run()
